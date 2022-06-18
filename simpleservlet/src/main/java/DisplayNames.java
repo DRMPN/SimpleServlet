40e59b3b187b43a2c8ab9c6f56c291b08ec747d6
DRMPN <mr.gorden.smit@gmail.com>
2022-06-18 15:02:32 +0300

    new servlet version

--- simpleservlet/src/main/java/DisplayNames.java
+++ simpleservlet/src/main/java/DisplayNames.java
@@ -2,17 +2,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/names")
 public class DisplayNames extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter pw = resp.getWriter();
-        pw.println("Simple Servlet");
+        pw.println("<h1> Simple Servlet </h1>");
     }
 
 }
